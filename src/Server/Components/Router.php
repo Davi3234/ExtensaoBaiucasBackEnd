d6ffd8dab91d8df4384e739d9d20cc7339b5a142
Dan Ruan <danrsaquetti@gmail.com>
2024-08-21 08:18:51 -0300

    create routers method

--- src/Server/Components/Router.php
+++ src/Server/Components/Router.php
@@ -3,7 +3,40 @@
 namespace App\Core\Components;
 
 class Router {
+    /**
+     * @var array{
+     * GET: array<string, array{handlers: array}>,
+     * POST: array<string, array{handlers: array}>,
+     * PUT: array<string, array{handlers: array}>,
+     * DELETE: array<string, array{handlers: array}>,
+     * }
+     */
+    protected static $routers = [
+        'GET' => [],
+        'POST' => [],
+        'PUT' => [],
+        'DELETE' => [],
+    ];
 
-    static function get() {
+    static function get($path, ...$handlers) {
+        self::createRouter('GET', $path, $handlers);
+    }
+
+    static function post($path, ...$handlers) {
+        self::createRouter('POST', $path, $handlers);
+    }
+
+    static function put($path, ...$handlers) {
+        self::createRouter('PUT', $path, $handlers);
+    }
+
+    static function delete($path, ...$handlers) {
+        self::createRouter('DELETE', $path, $handlers);
+    }
+
+    protected static function createRouter($method, $path, $handlers) {
+        self::$routers[$method][$path] = [
+            'handlers' => $handlers,
+        ];
     }
 }
