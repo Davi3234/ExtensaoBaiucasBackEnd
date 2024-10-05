8aabe41dca33b4020202d9913efa0e02ba7c2106
Dan Ruan <danrsaquetti@gmail.com>
2024-10-05 16:15:16 -0300

    update attribute status code HttpException

--- src/Exception/Http/HttpException.php
+++ src/Exception/Http/HttpException.php
@@ -6,9 +6,12 @@ use App\Core\Components\Result;
 use App\Exception\RuntimeException;
 
 class HttpException extends RuntimeException {
-  private readonly int $statusCode;
 
-  function __construct(string $message, int $statusCode, array $causes = []) {
+  function __construct(
+    string $message,
+    private readonly int $statusCode,
+    array $causes = []
+  ) {
     parent::__construct($message, $causes);
 
     $this->statusCode = $statusCode;
