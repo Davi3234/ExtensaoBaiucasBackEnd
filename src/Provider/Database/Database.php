132c8f86982f4f0d95c6e2053749392b813cc463
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2024-10-08 13:23:04 -0300

    Update Database.php

--- src/Provider/Database/Database.php
+++ src/Provider/Database/Database.php
@@ -12,6 +12,8 @@ use App\Exception\Exception;
  */
 class Database extends DatabaseConnection implements IDatabase {
 
+  error_code = pg_result_error_field($qryClientStatus, PGSQL_DIAG_SQLSTATE);
+
   #[\Override]
   function execFromSqlBuilder(SQLBuilder $sqlBuilder): array|bool {
     $sql = $sqlBuilder->build();
