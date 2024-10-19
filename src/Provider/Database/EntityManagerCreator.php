fac3580c634178a451bbf7c6fcb2824b5a660024
Dan Ruan <danrsaquetti@gmail.com>
2024-10-19 15:50:10 -0300

    build: Atualizando as variaveis de ambiente

--- src/Provider/Database/EntityManagerCreator.php
+++ src/Provider/Database/EntityManagerCreator.php
@@ -6,40 +6,36 @@ use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 
-class EntityManagerCreator
-{
-    protected static $instance;
-    private $entityManager;
+class EntityManagerCreator {
+  protected static $instance;
+  private $entityManager;
 
-    private function __construct()
-    {
-        $config = ORMSetup::createAttributeMetadataConfiguration(
-            paths: [__DIR__ . '/../../Model'],
-            isDevMode: true,
-        );
+  private function __construct() {
+    $config = ORMSetup::createAttributeMetadataConfiguration(
+      paths: [__DIR__ . '/../../Model'],
+      isDevMode: true,
+    );
 
-        $connection = DriverManager::getConnection([
-            'driver' => 'pdo_pgsql',
-            'dbname' => getenv('DBNAME'),
-            'user' => getenv('USER'),
-            'password' => getenv('PASSWORD'),
-            'host' => getenv('HOST'),
-            'port' => getenv('PORT')
-        ], $config);
+    $connection = DriverManager::getConnection([
+      'driver' => getenv('DB_DRIVER'),
+      'dbname' => getenv('DB_DATABASE'),
+      'host' => getenv('DB_HOST'),
+      'port' => getenv('DB_PORT'),
+      'user' => getenv('DB_USERNAME'),
+      'password' => getenv('DB_PASSWORD'),
+    ], $config);
 
-        $this->entityManager = new EntityManager($connection, $config);
+    $this->entityManager = new EntityManager($connection, $config);
+  }
+
+  public static function getInstance(): self {
+    if (self::$instance === null) {
+      self::$instance = new EntityManagerCreator();
     }
+    return self::$instance;
+  }
 
-    public static function getInstance(): self
-    {
-        if (self::$instance === null) {
-            self::$instance = new EntityManagerCreator();
-        }
-        return self::$instance;
-    }
-
-    public function getEntityManager(): EntityManager
-    {
-        return $this->entityManager;
-    }
+  public function getEntityManager(): EntityManager {
+    return $this->entityManager;
+  }
 }
