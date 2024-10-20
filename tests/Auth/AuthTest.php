77b9d1f9f6ecc57762d8aff193fc1a006d1fefec
Dan Ruan <danrsaquetti@gmail.com>
2024-10-19 23:12:37 -0300

    refator: Aplicando Mock nos testes de Autenticação

--- tests/Auth/AuthTest.php
+++ tests/Auth/AuthTest.php
@@ -10,31 +10,6 @@ use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use App\Service\AuthService;
 
-class UserRepositoryMock implements IUserRepository {
-  function create(User $user): User {
-    throw new CriticalException('Method not implemented');
-  }
-  function update(User $user): User {
-    throw new CriticalException('Method not implemented');
-  }
-  function deleteById(int $id): User {
-    throw new CriticalException('Method not implemented');
-  }
-
-  /**
-   * @return User[]
-   */
-  function findMany(): array {
-    throw new CriticalException('Method not implemented');
-  }
-  function findByLogin(string $login): ?User {
-    return User::__loadModel(['id' => 1, 'name' => 'Dan Ruan', 'login' => $login, 'password' => md5('Abc123!@#')]);
-  }
-  function findById(int $id): ?User {
-    throw new CriticalException('Method not implemented');
-  }
-}
-
 class AuthTest extends TestCase {
 
   #[Test]
@@ -43,8 +18,12 @@ class AuthTest extends TestCase {
     $login = 'dan@gmail.com';
     $password = 'Abc123!@#';
 
+    $userRepository = TestCase::createMock(IUserRepository::class);
+
+    $userRepository->method('findByLogin')->with($login)->willReturn(User::__loadModel(['id' => 1, 'name' => 'Dan Ruan', 'login' => $login, 'password' => md5('Abc123!@#')]));
+
     // Action
-    $authService = new AuthService(new UserRepositoryMock());
+    $authService = new AuthService($userRepository);
 
     $result = $authService->login([
       'login' => $login,
