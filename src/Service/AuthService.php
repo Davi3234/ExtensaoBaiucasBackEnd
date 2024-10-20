bf618b333ea94e5f6ffcb64741c68cca6e936b11
Dan Ruan <danrsaquetti@gmail.com>
2024-10-19 21:00:15 -0300

    feat: Criando payload JWT na autenticação

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -2,9 +2,17 @@
 
 namespace App\Service;
 
+use App\Exception\Http\BadRequestException;
 use App\Provider\Zod\Z;
+use App\Repository\IUserRepository;
+use Firebase\JWT\JWT;
 
 class AuthService {
+
+  function __construct(
+    private readonly IUserRepository $userRepository
+  ) {
+  }
 
   function login(array $args) {
     $loginSchema = Z::object([
@@ -14,6 +22,21 @@ class AuthService {
 
     $dto = $loginSchema->parseNoSafe($args);
 
-    return ['message' => 'Teste'];
+    $user = $this->userRepository->findByLogin($dto->login);
+
+    if (!$user) {
+      throw new BadRequestException('Login or password invalid');
+    }
+
+    $payload = [
+      'exp' => time() + 10,
+      'iat' => time(),
+      'sub' => $user->getId(),
+      'name' => $user->getName(),
+    ];
+
+    $token = JWT::encode($payload, env('APP_KEY'), 'HS256');
+
+    return ['message' => 'Teste', 'payload' => $payload, 'token' => $token];
   }
 }
--- tests/Auth/AuthTest.php
+++ tests/Auth/AuthTest.php
@@ -2,9 +2,37 @@
 
 namespace Tests\User;
 
+use App\Exception\CriticalException;
+use App\Model\User;
+use App\Repository\IUserRepository;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use App\Service\AuthService;
+
+class UserRepositoryMock implements IUserRepository {
+  function create(User $user): User {
+    throw new CriticalException('Method not implemented');
+  }
+  function update(User $user): User {
+    throw new CriticalException('Method not implemented');
+  }
+  function deleteById(int $id): User {
+    throw new CriticalException('Method not implemented');
+  }
+
+  /**
+   * @return User[]
+   */
+  function findMany(): array {
+    throw new CriticalException('Method not implemented');
+  }
+  function findByLogin(string $login): ?User {
+    return User::__loadModel(['id' => 1, 'name' => 'Dan Ruan', 'login' => $login]);
+  }
+  function findById(int $id): ?User {
+    throw new CriticalException('Method not implemented');
+  }
+}
 
 class AuthTest extends TestCase {
 
@@ -13,13 +41,13 @@ class AuthTest extends TestCase {
     $login = 'dan@gmail.com';
     $password = 'Dan123!@#';
 
-    $authService = new AuthService();
+    $authService = new AuthService(new UserRepositoryMock());
 
     $result = $authService->login([
       'login' => $login,
       'password' => $password,
     ]);
 
-    $this->assertEquals($result['message'], 'Teste');
+    $this->assertEquals($result['payload']['sub'], 1);
   }
 }
