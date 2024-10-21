b01c3d56902017495dc07ce583cd7344ada8d107
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2024-10-21 19:56:59 -0300

    build: Mudanças no phpunit.xml por não ter configuração para cobertura de código e no sh que executa os testes antes de o container iniciar

--- config/configs.sh
+++ config/configs.sh
@@ -3,6 +3,15 @@ set -x
 
 #VERIFICAR SEMPRE SE A CODIFICAÇÃO DO ARQUIVO ESTÁ LF
 
+php vendor/bin/phpunit Tests --colors
+
+if $? -ne 2 then
+  echo "Algum teste falhou";
+  exit 1;
+fi
+
+echo "Todos os testes passaram";
+
 php /var/www/html/bin/doctrine orm:schema-tool:create
 
 php -S 0.0.0.0:80 -t /var/www/html
