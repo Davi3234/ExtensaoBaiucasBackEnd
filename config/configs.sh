bf023fa62840e3529ccef0990b474613dc87dfcc
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2024-10-22 19:56:21 -0300

    build: Arrumando o configs.sh para rodar os testes antes de inicializar o container docker

--- config/configs.sh
+++ config/configs.sh
@@ -5,12 +5,12 @@ set -x
 
 php vendor/bin/phpunit Tests --colors
 
-if $? -ne 2 then
-  echo "Algum teste falhou";
-  exit 1;
+if $? -ne 2; then
+  echo "Algum teste falhou"
+  exit 1
 fi
 
-echo "Todos os testes passaram";
+echo "Todos os testes passaram"
 
 php /var/www/html/bin/doctrine orm:schema-tool:create
 
