3c38dcb84bee953123e74e8f82f0c5727a91bc24
Dan Ruan <danrsaquetti@gmail.com>
2024-10-26 15:02:23 -0300

    refactor: Copiando os arquivos referente à Requisição HTTP para a nova arquitetura

---
+++ core/HTTP/Request.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Core\HTTP;
+
+class Request {
+
+  function __construct(
+    private readonly array $body = [],
+    private readonly array $params = [],
+    private array $attributes = [],
+    private readonly string $router = '/',
+    private readonly string $method = ''
+  ) {
+  }
+
+  function getParams() {
+    return $this->params;
+  }
+
+  function getParam(string $name) {
+    return $this->params[$name] ?: null;
+  }
+
+  function getHeaders() {
+    return $_SERVER;
+  }
+
+  function getHeader(string $name) {
+    return $_SERVER[$name] ?: null;
+  }
+
+  function getRouter() {
+    return $this->router;
+  }
+
+  function getMethod() {
+    return $this->method;
+  }
+
+  function getAllBody() {
+    return $this->body;
+  }
+
+  function getBody(string $name) {
+    return $this->body[$name] ?: null;
+  }
+
+  function getAttributes() {
+    return $this->attributes;
+  }
+
+  function getAttribute(string $name) {
+    return $this->attributes[$name] ?: null;
+  }
+
+  function setAttribute(string $name, $value) {
+    $this->attributes[$name] = $value;
+  }
+
+  static function getPathHttpRequested() {
+    if (!isset($_GET['url']))
+      $_GET['url'] = $_SERVER['PATH_INFO'];
+
+    if (!$_GET['url'])
+      $_GET['url'] = '/';
+
+    $_GET['url'] = str_replace('//', '/', $_GET['url']);
+
+    return $_GET['url'];
+  }
+
+  static function getMethodHttpRequested() {
+    return $_SERVER['REQUEST_METHOD'] ?? null;
+  }
+
+  static function getBodyRequest() {
+    $dataJson = file_get_contents('php://input');
+    return json_decode($dataJson, true) ?? [];
+  }
+
+  static function getParamsRequest() {
+    return $_GET ?? [];
+  }
+}
---
+++ core/HTTP/Response.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Core\HTTP;
+
+use Core\Common\Result;
+
+class Response {
+  private $dataResponse = null;
+
+  function __construct() {
+    header('charset=UTF-8');
+  }
+
+  function status(int $status) {
+    http_response_code($status);
+    return $this;
+  }
+
+  function setDataResponse($dataResponse) {
+    $this->dataResponse = $dataResponse;
+  }
+
+  function getDataResponse() {
+    return $this->dataResponse;
+  }
+
+  function sendDataResponse(int $status = null) {
+    if ($status)
+      $this->status($status);
+
+    $this->sendJson($this->dataResponse, $status);
+  }
+
+  function sendJson(array|object $data, int $status = null) {
+    if ($data instanceof Result) {
+      $status = $data->getStatus();
+      $data = $data->getResult();
+    }
+
+    if ($status)
+      $this->status($status);
+
+    if (is_object($data))
+      $data = (object)(array)$data;
+
+    header('Content-Type: application/json');
+    $data = json_encode($data);
+
+    exit($data);
+  }
+}
