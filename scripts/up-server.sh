347b5c9ab35e6bdefff263ddb5d8a239053250d3
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2024-11-03 19:57:25 -0300

    build: Mudanças no dockercompose e script

--- scripts/up-server.sh
+++ scripts/up-server.sh
@@ -10,6 +10,6 @@ fi
 
 echo "Todos os testes passaram"
 
-php /var/www/html/bin/doctrine orm:schema-tool:create
+php /var/www/html/bin/doctrine orm:schema-tool:update --force
 
-php -S 0.0.0.0:80 -t /var/www/html
+php -S 0.0.0.0:80 -t /var/www/html/ public/index.php
