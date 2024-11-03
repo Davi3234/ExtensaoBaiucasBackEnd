db256323a9d22ca260e86cc5607075a604e6a25e
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2024-11-03 20:21:09 -0300

    build: Mudanças no docker-compose, Dockerfile e script de inicialização do backend

--- scripts/up-server.sh
+++ scripts/up-server.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -x
 
-php vendor/bin/phpunit Tests --colors
-
-if [ $? -ne 0 ]; then
-  echo "Algum teste falhou"
-  exit 1
-fi
-
-echo "Todos os testes passaram"
-
 php /var/www/html/bin/doctrine orm:schema-tool:update --force
 
 php -S 0.0.0.0:80 -t /var/www/html/ public/index.php
