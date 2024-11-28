9b377f99efb0763bbd7cca28678affd3f42eb2b4
Dan Ruan <danrsaquetti@gmail.com>
2024-11-27 23:43:01 -0300

    fix: Atualizando schema de valização Zod no método update do UserService

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -85,7 +85,7 @@ class UserService {
       'cpf' => Z::string(['required' => 'CPF é de preenchimento obrigatório'])
         ->trim()
         ->refine(
-          function($cpf){
+          function ($cpf) {
             $cpf = preg_replace('/\D/', '', $cpf);
 
             if (strlen($cpf) != 11) {
@@ -111,7 +111,9 @@ class UserService {
             }
 
             return true;
-          }, 'CPF é inválido'),
+          },
+          'CPF é inválido'
+        ),
       'endereco' => Z::string(['required' => 'Endereço é de preenchimento obrigatório'])
         ->trim(),
       'password' => Z::string(['required' => 'Senha é de preenchimento obrigatório'])
@@ -176,13 +178,44 @@ class UserService {
         ->regex('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', 'Login inválido'),
       'cpf' => Z::string()
         ->optional()
-        ->trim(),
+        ->trim()
+        ->refine(
+          function ($cpf) {
+            $cpf = preg_replace('/\D/', '', $cpf);
+
+            if (strlen($cpf) != 11) {
+              return false;
+            }
+
+            if (preg_match('/^(\d)\1*$/', $cpf)) {
+              return false;
+            }
+
+            for ($t = 9; $t < 11; $t++) {
+              $soma = 0;
+              for ($i = 0; $i < $t; $i++) {
+                $soma += $cpf[$i] * (($t + 1) - $i);
+              }
+
+              $digito = ($soma * 10) % 11;
+              $digito = ($digito == 10 || $digito == 11) ? 0 : $digito;
+
+              if ($digito != $cpf[$t]) {
+                return false;
+              }
+            }
+
+            return true;
+          },
+          'CPF é inválido'
+        ),
       'endereco' => Z::string()
         ->optional()
         ->trim(),
       'password' => Z::string()
         ->optional()
-        ->trim(),
+        ->min(8, 'Utilize ao menos 8 caracteres')
+        ->regex('/^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).*$/', 'Utilize ao menos um símbolo especial, uma letra maiúscula e um número'),
       'confirm_password' => Z::string()
         ->optional()
         ->trim(),
@@ -194,7 +227,7 @@ class UserService {
     ])
       ->coerce()
       ->refine(function ($value) {
-        return $value->password != $value->confirm_password;
+        return $value->password == $value->confirm_password;
       }, 'A nova senha deve ser igual a confirmação de senha');
 
     $dto = $updateSchema->parseNoSafe($args);
