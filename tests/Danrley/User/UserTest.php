3d240308732d2dae2ff06e613a198599c04853d9
Dan Ruan <danrsaquetti@gmail.com>
2024-11-27 23:33:41 -0300

    feat: Criando teste naoDeveAtualizarUsuarioComSenhaInvalido

--- tests/Danrley/User/UserTest.php
+++ tests/Danrley/User/UserTest.php
@@ -88,4 +88,45 @@ class UserTest extends TestCase {
       throw $err;
     }
   }
+
+  #[Test]
+  public function naoDeveAtualizarUsuarioComSenhaInvalido() {
+    $dto = [
+      'id' => 1,
+      'name' => 'Dan Ruan',
+      'login' => '',
+      'cpf' => '645.549.820-84',
+      'endereco' => 'Rua de Teste 2',
+      'password' => 'Davi432',
+      'confirm_password' => 'Davi432',
+    ];
+
+    $userRepository = $this->createMock(IUserRepository::class);
+
+    $userRepository
+      ->method('findById')
+      ->with($dto['id'])
+      ->willReturn(new User(
+        id: 1,
+        name: 'Dan Ruan',
+        login: 'dan@gmail.com',
+        cpf: '489.945.080-07',
+        endereco: 'Rua de Teste',
+        password: 'Dan!@#123',
+        active: true,
+        tipo: TipoUsuario::ADMNISTRADOR,
+      ));
+
+    $userService = new UserService($userRepository);
+
+    $this->expectException(ZodParseException::class);
+
+    try {
+      $userService->update($dto);
+    } catch (Exception $err) {
+      $this->assertNotEmpty($err->getCausesFromOrigin('password'));
+
+      throw $err;
+    }
+  }
 }
