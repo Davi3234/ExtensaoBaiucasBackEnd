9880ee6c5b076af16b0fc7b3ccd022762da20596
Daiane de Oliveira <daiane.oliveira@siscobra.com.br>
2024-12-01 16:43:25 -0300

    salvando alterações

--- tests/Daiane/Unitario/OrderTest.php
+++ tests/Daiane/Unitario/OrderTest.php
@@ -977,33 +977,4 @@ class OrderTest extends TestCase
     $this->assertEquals(StatusPedido::CANCELADO, $pedido->status);
     $this->assertEquals(['message' => 'Pedido atualizado com sucesso'], $resultados);
   }
-
-  ///////////////////////////////////////////Testes de Integração/////////////////////////////////////////////////
-
-  /*#[Test]
-  public function GetPedidos()
-  {
-
-    //Caso de teste 01 (Integração): Verificar se o pedido com um único item é recuperado corretamente
-
-    //Aqui teste 01
-
-    //Caso de teste 02 (Integração): Verificar se o pedido com múltiplos itens é recuperado corretamente
-
-    //Aqui teste 02
-
-    //Caso de teste 03 (Integração): Verificar se ao informar um pedido com 0 itens o método retorna erro
-
-    //Aqui teste 03
-  }
-
-  #[Test]
-  public function  CreatePedido()
-  {
-
-    //Caso de teste 01 (Integração): Garantir que o pedido seja inserido corretamente no banco de dados com todas as informações do pedido informadas
-
-    //Aqui teste 01
-
-  }*/
 }
