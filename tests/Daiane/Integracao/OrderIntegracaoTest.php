42b3adc42a116b4e8616dffb5d282359918398ec
Daiane de Oliveira <daiane.oliveira@siscobra.com.br>
2024-12-01 16:42:37 -0300

    Salva mudanças em composer.json e OrderTest.php

---
+++ tests/Daiane/Integracao/OrderIntegracaoTest.php
@@ -0,0 +1,30 @@
+<?php
+
+///////////////////////////////////////////Testes de Integração/////////////////////////////////////////////////
+
+/*#[Test]
+public function GetPedidos()
+{
+
+  //Caso de teste 01 (Integração): Verificar se o pedido com um único item é recuperado corretamente
+
+  //Aqui teste 01
+
+  //Caso de teste 02 (Integração): Verificar se o pedido com múltiplos itens é recuperado corretamente
+
+  //Aqui teste 02
+
+  //Caso de teste 03 (Integração): Verificar se ao informar um pedido com 0 itens o método retorna erro
+
+  //Aqui teste 03
+}
+
+#[Test]
+public function  CreatePedido()
+{
+
+  //Caso de teste 01 (Integração): Garantir que o pedido seja inserido corretamente no banco de dados com todas as informações do pedido informadas
+
+  //Aqui teste 01
+
+}*/
